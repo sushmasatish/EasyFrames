dc6aac7afdd20b4f4fb5acf94edd7a9d0c1abd25
Sushma Satish <sush@iSush.local>
2015-05-29 20:59:50 +0100

    Initial Commit

---
+++ EasyFrames/EasyFrames.h
@@ -0,0 +1,13 @@
+//
+//  EasyFrames.h
+//  EasyFrames
+//
+//  Created by Sushma Satish on 29/05/2015.
+//  Copyright (c) 2015 Appsify. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EasyFrames : NSObject
+
+@end
---
+++ EasyFrames/EasyFrames.m
@@ -0,0 +1,13 @@
+//
+//  EasyFrames.m
+//  EasyFrames
+//
+//  Created by Sushma Satish on 29/05/2015.
+//  Copyright (c) 2015 Appsify. All rights reserved.
+//
+
+#import "EasyFrames.h"
+
+@implementation EasyFrames
+
+@end
